3aafb13330d721bfb3924d7fb4b5fadde91e4aee
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-09-02 23:20:57 -0300

    avocado.virt: Initial code import
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

---
+++ setup.py
@@ -0,0 +1,29 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Copyright: Red Hat Inc. 2013-2014
+# Author: Lucas Meneghel Rodrigues <lmr@redhat.com>
+
+# pylint: disable=E0611
+from distutils.core import setup
+
+VERSION = '0.0.1'
+
+setup(name='avocado',
+      version=VERSION,
+      description='Avocado Virtualization Testing Plugin',
+      author='Lucas Meneghel Rodrigues',
+      author_email='lmr@redhat.com',
+      url='http://github.com/avocado-framework/avocado-virt',
+      packages=['avocado',
+                'avocado.virt',
+                'avocado.plugins'],
+      )
