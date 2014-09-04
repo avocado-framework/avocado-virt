487ed52d53969f27c3bd3702052f68cc026a1f04
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-09-04 18:32:09 -0300

    avocado-virt: Fix license files
    My mistake, I was rushing to deliver this on time for the
    sprint review.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- avocado/virt/qemu/devices.py
+++ avocado/virt/qemu/devices.py
@@ -1,3 +1,22 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See LICENSE for more details.
+#
+# Inspired in original code by IBM Corp and Red Hat Inc.
+#
+# Copyright (C) 2012 IBM Corp.
+# Copyright (C) 2014 Red Hat Inc
+#
+# Author: Lucas Meneghel Rodrigues <lmr@redhat.com>
+# Author: Stefan Hajnoczi <stefanha@redhat.com>
+
 from avocado.utils import network
 from avocado.virt.qemu import path
 
