af9a34bc30a36ba0f082ee94a8ee9206318134bc
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-09-18 11:24:00 -0300

    avocado.plugins.virt: Add virt related parser group
    This will organize the virt options better, helping
    the logical display of command line arguments.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- avocado/plugins/virt.py
+++ avocado/plugins/virt.py
@@ -36,35 +36,35 @@ class VirtOptions(plugin.Plugin):
     app_logger = logging.getLogger('avocado.app')
 
     def configure(self, parser):
-        self.parser = parser
-        self.parser.runner.add_argument(
+        virt_parser = parser.runner.add_argument_group('virtualization testing arguments')
+        virt_parser.add_argument(
             '--qemu-bin', type=str,
             dest='qemu_bin',
             help=('Path to a custom qemu binary to be tested. Default path: %s'
                   % defaults.qemu_bin))
-        self.parser.runner.add_argument(
+        virt_parser.add_argument(
             '--qemu-dst-bin', type=str,
             dest='qemu_dst_bin',
             help=('Path to a destination qemu binary to be tested. Used as '
                   'incoming qemu in migration tests. Default path: %s'
                   % defaults.qemu_dst))
-        self.parser.runner.add_argument(
+        virt_parser.add_argument(
             '--guest-image-path', type=str,
             dest='guest_image_path',
             help=('Path to a guest image to be used in tests. Default path: %s'
                   % defaults.guest_image_path))
-        self.parser.runner.add_argument(
+        virt_parser.add_argument(
             '--disable-restore-image-test', action='store_true',
             default=defaults.disable_restore_image_test,
             dest='disable_restore_image_test',
             help=('Do not restore the guest image before individual tests '
                   'start. Default: %s' % defaults.disable_restore_image_test))
-        self.parser.runner.add_argument(
+        virt_parser.add_argument(
             '--disable-restore-image-job', action='store_true',
             default=defaults.disable_restore_image_job,
             dest='disable_restore_image_job',
             help=('Do not restore the guest image before a test job '
-                  'start. Default: %s' % defaults.disable_restore_image_job))
+                  'starts. Default: %s' % defaults.disable_restore_image_job))
 
         self.configured = True
 
