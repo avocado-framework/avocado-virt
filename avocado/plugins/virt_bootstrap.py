71f29c5378ffd8640b4f630add5a0f116c4e463a
Lucas Meneghel Rodrigues <lmr@redhat.com>
2014-10-06 17:25:17 -0300

    avocado.plugins.virt_bootstrap: Change message priorities
    Change message priorities for better output. Also, re-phrase
    a number of messages for clarity.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- avocado/plugins/virt_bootstrap.py
+++ avocado/plugins/virt_bootstrap.py
@@ -14,7 +14,6 @@
 
 import os
 import urllib2
-import logging
 
 from avocado.plugins import plugin
 from avocado.core import output
@@ -41,24 +40,32 @@ class VirtBootstrap(plugin.Plugin):
         super(VirtBootstrap, self).configure(self.parser)
 
     def run(self, args):
+        fail = False
         view = output.View(app_args=args)
-        bcolors = output.term_support
-        view.notify(event='message', msg="Verifying if you have '7za' installed")
+        view.notify(event='message', msg='Probing your system for test requirements')
         try:
             process.find_command('7za')
+            view.notify(event='minor', msg='7zip present')
         except process.CmdNotFoundError:
-            view.notify(event='message', msg="7za not installed. You can try to install 'p7zip' to get it.")
+            view.notify(event='warning',
+                        msg=("7za not installed. You may "
+                             "install 'p7zip' (or the "
+                             "equivalent on your distro) to "
+                             "fix the problem"))
+            fail = True
 
         jeos_sha1_url = 'https://lmr.fedorapeople.org/jeos/SHA1SUM_JEOS20'
-        view.notify(event='message', msg="Checking if JeOS is in the right location and matching SHA1")
         try:
-            view.notify(event='message', msg="Verifying expected SHA1 sum from %s" % jeos_sha1_url)
+            view.notify(event='minor',
+                        msg=('Verifying expected SHA1 '
+                             'sum from %s' % jeos_sha1_url))
             sha1_file = urllib2.urlopen(jeos_sha1_url)
             sha1_contents = sha1_file.read()
             sha1 = sha1_contents.split(" ")[0]
-            view.notify(event='message', msg="Expected SHA1 sum: %s" % sha1)
+            view.notify(event='minor', msg='Expected SHA1 sum: %s' % sha1)
         except Exception, e:
-            view.notify(event='error', msg="Failed to get SHA1 from file: %s" % e)
+            view.notify(event='error', msg='Failed to get SHA1 from file: %s' % e)
+            fail = True
 
         jeos_dst_dir = path.init_dir(os.path.join(data_dir.get_data_dir(),
                                                   'images'))
@@ -72,23 +79,46 @@ class VirtBootstrap(plugin.Plugin):
 
         if actual_sha1 != sha1:
             if actual_sha1 == '0':
-                view.notify(event='message', msg="JeOS could not be found at %s. "
-                            "Downloading it (173 MB). Please wait..." %
-                            jeos_dst_path)
+                view.notify(event='minor',
+                            msg=('JeOS could not be found at %s. Downloading '
+                                 'it (173 MB). Please wait...' % jeos_dst_path))
             else:
-                view.notify(event='message', msg="JeOS at %s is either corrupted or outdated. "
-                            "Downloading a new copy (173 MB). Please wait..." %
-                            jeos_dst_path)
+                view.notify(event='minor',
+                            msg=('JeOS at %s is either corrupted or outdated. '
+                                 'Downloading a new copy (173 MB). '
+                                 'Please wait...' % jeos_dst_path))
             jeos_url = 'https://lmr.fedorapeople.org/jeos/jeos-20-64.qcow2.7z'
             try:
                 download.url_download(jeos_url, jeos_dst_path)
             except:
-                view.notify(event='message', msg="Exiting upon user "
-                            "request (Download "
-                            "not finished)")
+                view.notify(event='warning',
+                            msg=('Exiting upon user request (Download '
+                                 'not finished)'))
         else:
-            view.notify(event='message', msg="Image found, with proper SHA1")
+            view.notify(event='minor',
+                        msg=('Compressed JeOS image found '
+                             'in %s, with proper SHA1' % jeos_dst_path))
 
-        view.notify(event='message', msg="Uncompressing the image...")
+        view.notify(event='minor',
+                    msg=('Uncompressing the JeOS image to restore pristine '
+                         'state. Please wait...'))
         os.chdir(os.path.dirname(jeos_dst_path))
-        process.run('7za -y e %s' % os.path.basename(jeos_dst_path))
+        result = process.run('7za -y e %s' % os.path.basename(jeos_dst_path),
+                             ignore_status=True)
+        if result.exit_status != 0:
+            view.notify(event='error',
+                        msg=('Error uncompressing the image '
+                             '(see details below):\n%s' % result))
+            fail = True
+        else:
+            view.notify(event='minor', msg='Successfully uncompressed the image')
+
+        if fail:
+            view.notify(event='warning',
+                        msg=('Problems found probing this system for tests '
+                             'requirements. Please check the error messages '
+                             'and fix the problems found'))
+        else:
+            view.notify(event='message',
+                        msg=('Your system appears to be all '
+                             'set to execute tests'))
