be7894e23464e7f09b3a3bb89ff7377d3d276940
Lucas Meneghel Rodrigues <lmr@redhat.com>
2015-01-09 17:21:52 -0200

    avocado.virt.defaults: Get defaults from config file
    Modify the defaults module to ensure the defaults will
    come from the avocado virt config file.
    
    Signed-off-by: Lucas Meneghel Rodrigues <lmr@redhat.com>

--- avocado/virt/defaults.py
+++ avocado/virt/defaults.py
@@ -17,41 +17,59 @@ Default values used in tests and plugin code.
 """
 
 from avocado.core import data_dir
+from avocado.settings import settings
+from avocado.settings import SettingsError
 from avocado.virt.qemu import path
 
 try:
-    qemu_bin = path.get_qemu_binary()
-except:
-    qemu_bin = 'qemu'
+    qemu_bin = settings.get_value('virt.qemu.paths', 'qemu_bin')
+except SettingsError:
+    try:
+        qemu_bin = path.get_qemu_binary()
+    except path.QEMUCmdNotFoundError:
+        qemu_bin = 'qemu'
 
 try:
-    qemu_dst = path.get_qemu_dst_binary()
-except:
-    qemu_dst = 'qemu'
+    qemu_dst = settings.get_value('virt.qemu.paths', 'qemu_dst_bin')
+except SettingsError:
+    try:
+        qemu_dst = path.get_qemu_dst_binary()
+    except path.QEMUCmdNotFoundError:
+        qemu_dst = 'qemu'
 
 try:
-    qemu_img_bin = path.get_qemu_img_binary()
-except:
-    qemu_img_bin = 'qemu-img'
+    qemu_img_bin = settings.get_value('virt.qemu.paths', 'qemu_img_bin')
+except SettingsError:
+    try:
+        qemu_img_bin = path.get_qemu_img_binary()
+    except path.QEMUCmdNotFoundError:
+        qemu_img_bin = 'qemu-img'
 
 try:
-    qemu_io_bin = path.get_qemu_io_binary()
-except:
-    qemu_io_bin = 'qemu-io'
+    qemu_img_bin = settings.get_value('virt.qemu.paths', 'qemu_io_bin')
+except SettingsError:
+    try:
+        qemu_io_bin = path.get_qemu_io_binary()
+    except path.QEMUCmdNotFoundError:
+        qemu_io_bin = 'qemu-io'
 
 # The defaults are related to the default image used (JeOS)
-guest_image_path = data_dir.get_datafile_path('images',
-                                              'jeos-20-64.qcow2')
-guest_user = 'root'
-guest_password = '123456'
+try:
+    qemu_img_bin = settings.get_value('virt.guest', 'image_path')
+except SettingsError:
+    guest_image_path = data_dir.get_datafile_path('images',
+                                                  'jeos-20-64.qcow2')
 
-disable_restore_image_test = False
-disable_restore_image_job = False
+guest_user = settings.get_value('virt.guest', 'user', default='root')
+guest_password = settings.get_value('virt.guest', 'password', default='123456')
 
-screendump_thread_enable = False
-screendump_thread_interval = 0.5
+disable_restore_image_test = settings.get_value('virt.restore', 'disable_for_test', default=False)
+disable_restore_image_job = settings.get_value('virt.restore', 'disable_for_job', default=False)
 
-video_encoding_enable = False
-video_encoding_jpeg_quality = 95
+screendump_thread_enable = settings.get_value('virt.screendumps', 'enable', default=False)
+screendump_thread_interval = settings.get_value('virt.screendumps', 'interval', default=0.5)
 
-migrate_timeout = 60.0
+video_encoding_enable = settings.get_value('virt.videos', 'enable', default=False)
+video_encoding_jpeg_quality = settings.get_value('virt.videos', 'jpeg_conversion_quality', default=95)
+
+migrate_timeout = settings.get_value('virt.qemu.migrate', 'timeout', default=60.0)
